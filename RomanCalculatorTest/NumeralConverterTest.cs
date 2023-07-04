709cbd177fc4f0ab878f4b00c79b52059b14906d
Andre Pries <a.pries@quat2ro.com>
2023-07-04 11:12:13 +0200

    Numerals: added complex tests

--- RomanCalculatorTest/NumeralConverterTest.cs
+++ RomanCalculatorTest/NumeralConverterTest.cs
@@ -17,10 +17,22 @@ namespace RomanCalculatorTest
         }
 
         [TestMethod]
-        public void TestNCheckNumeralSimpleTrue()
+        public void TestNCheckNumeralSimple()
         {
             List<string> TestCases = new List<string>() { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
             List<int> Expected = new List<int> { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+
+            for (int i = 0; i < TestCases.Count; i++)
+            {
+                Assert.AreEqual(Converter.ConvertStringToInt(TestCases[i]), Expected[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestNCheckNumeralComplex()
+        {
+            List<string> TestCases = new List<string>() { "MDCLXVI", "MMMDXXXIII", "CIV", "CIX", "CML", "XIV", "MMMCMXCIX", "XCV", "CMX", "CMIV" };
+            List<int> Expected = new List<int> { 1666, 3533, 104, 109, 950, 14, 3999, 95, 910, 904 };
 
             for (int i = 0; i < TestCases.Count; i++)
             {
