4cd0b53dd6151e8b3910bd6b61e11491851e1cdb
Andre Pries <a.pries@quat2ro.com>
2023-07-04 10:40:43 +0200

    Added NumeralConverter interface and class

---
+++ RomanCalculator/Class/NumberConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanCalculator.Class
+{
+    internal class NumberConverter
+    {
+    }
+}
---
+++ RomanCalculator/Interface/NumeralConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanCalculator.Interface
+{
+    internal interface NumberConversion
+    {
+        public int ConvertStringToInt(string Number);
+
+        public string ConvertIntToString(int Number);
+    }
+}
